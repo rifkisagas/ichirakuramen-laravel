806d7fdf0cb789aff46abffc0d9b19b2ee35c4ce
Rifkisagas <arisagasr@gmail.com>
2022-12-02 11:18:06 +0700

    Index - Final Changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,13 +18,13 @@
             <img src="images/ichirakuramen.png">    
           </div>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#lists">Join our List</a></li>
-            <li><a href="#reservations">Reservations</a></li>
-            <li><a href="#events">Special Events</a></li>
-            <li><a href="#gallery">Gallery</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#menu">Menu</a></li>
+            <li><a href="#home" class="hvr-grow">Home</a></li>
+            <li><a href="#lists" class="hvr-grow">Join our List</a></li>
+            <li><a href="#reservations" class="hvr-grow">Reservations</a></li>
+            <li><a href="#events" class="hvr-grow">Special Events</a></li>
+            <li><a href="#gallery" class="hvr-grow">Gallery</a></li>
+            <li><a href="#contact" class="hvr-grow">Contact</a></li>
+            <li><a href="#menu" class="hvr-grow">Menu</a></li>
           </ul>
         </div>
       </nav>
@@ -37,54 +37,59 @@
       </div>
     </div>
   </header>
-  <div id="reservations" data-aos="fade-up">
-    <div class="reservations-img">
-
-    </div>
-    {{-- <h1>Test</h1> --}}
-  </div>
-  <div id="about" class="about">
-    <div class="about-title" data-aos="fade-up">
-      <h1 class="hvr-shrink">The World's Largest Cyber Security </h1>
-      <h1>Consultant Company.</h1>
-    </div>
-    <div class="about-desc" data-aos="fade-up">
-      <p>Secure your data and privacy with the most latest cyber security standards.</p>
-    </div>
-    <div class="about-port" data-aos="fade-down">
-      <div id="flowcarousel" class="carousel slide" data-bs-ride="carousel" data-interval="3000">
-        <ol class="carousel-indicators">
-          <li data-target="#flowcarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#flowcarousel" data-slide-to="1"></li>
-          <li data-target="#flowcarousel" data-slide-to="2"></li>
-        </ol>
-        {{-- <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="/images/hq.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>- Monitoring Room -</h5>
+  <div id="reservations">
+    <div class="container-xxl">
+      <div class="card text-bg-dark" data-aos="fade-up">
+        <img src="images/ramen-bowl.jpg" class="card-img" style="opacity: 0.7">
+        <div class="card-img-overlay">
+          <div class="row g-0">
+            <div class="card-body">
+              <div class="col-md-5" style="float:right;">
+                <div class="white-box box-pad" style="margin-top:40%;height:50%;" data-aos="fade-left">
+                  <div class="res-content" style="font-family: westmount-light;">
+                    <h5 class="card-title" style="margin-bottom:20px;font-size: 28px">Served from the bottom of heart</h5>
+                    <p class="card-text" style="font-size: 16px">Ichiraku, is a japanese restaurant and sushi bar offering a modern interpretation of Japanese cuisine. Featuring innovative and original creations, Ichiraku takes a bold and distinct approach in its menu, incorporating fresh and seasonal produce and ingrediens from the Hidden Leaf Village.</p>
+                  </div>
+                </div>
+              </div>
             </div>
+          {{-- <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small>Last updated 3 mins ago</small></p> --}}
           </div>
-          <div class="carousel-item">
-            <img src="/images/hq2.jpg" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>- Cyber Defence Opperation Headquarter -</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="events">
+    <div class="container-xxl">
+      <div class="white-box" data-aos="fade-down">
+        <div class="box-pad">
+          <div class="card-fluid" style="max-width: auto;">
+            <div class="row g-0">
+              <div class="col-md-5" style="">
+                <div class="card-body">
+                  <h5 class="card-title" style="font-family: westmount-light; margin-bottom:20px; font-size: 34px">Teuchi's Classic Ramen Shop</h5>
+                  <div class="card-text" style="font-family: westmount-light;">
+                    <p style="font-size: 16px">With classic design and originated look from konohagakure's ichiraku ramen shop, Ichiraku provides great service with a warm atmosphere from the Hidden Leaf Village. Ichiraku's name was given because it means <b style="font-family: westmount-regular">"Ramen is the Best Pleasure"</b>.</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6" style="margin-left: 80px;" data-aos="zoom-up">
+                <img src="images/ramen-shop.jpg" class="img-fluid rounded-start" alt="...">
+              </div>
             </div>
           </div>
         </div>
-        <a class="carousel-control-prev" href="#flowcarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        </a>
-        <a class="carousel-control-next" href="#flowcarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        </a> --}}
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas vero sequi similique officia est provident. Dicta quibusdam nam, odio eius voluptatum optio cumque natus, atque exercitationem accusantium a dolor necessitatibus.
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ratione suscipit inventore iste est. Ut dolor dolorum maiores quae quo nostrum deserunt, ab dolorem, molestiae veniam exercitationem ratione aperiam fugit.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, distinctio earum ratione magni porro dolor non necessitatibus eaque minima quisquam. Iusto quia dicta ut temporibus! Provident dolorum incidunt corrupti! Expedita!</h1>
       </div>
     </div>
   </div>
 </body>
+<footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+  <div class="container text-center">
+    <small>Copyright &copy; Ichiraku</small>
+  </div>
+</footer>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -93,7 +98,6 @@
   $('#wrapper-bg').fadeIn();
   $('#logo').fadeOut();
   AOS.init();
-
 
   $(window).on("scroll", function() {
     if($(window).scrollTop()) {
@@ -109,7 +113,7 @@
       $('nav ul li a').css({"color":"#AFAFAF"});
     }
   });
-  // $(window).on("scroll", function() {
+  
   //   if($(window).scrollTop() > 400 && $(this).scrollTop() < 800) {
       
   //   } else {
