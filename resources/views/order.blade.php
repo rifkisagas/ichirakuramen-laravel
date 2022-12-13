08878a520f188bd3b26498425a4f123fdb4d37e5
Rifkisagas <arisagasr@gmail.com>
2022-12-13 17:08:10 +0700

    Order - Dine in FORM Updates

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -69,23 +69,11 @@
               </div>
               <div class="input-group mb-3">
                 <button data-toggle="modal" data-target="#timemodal" class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="fa-regular fa-clock"></i></button>
-                <input type="text" placeholder="Time Reservation" class="form-control" id="basic-url" aria-describedby="basic-addon">
-              </div>
-              
-              <div class="input-group mb-3">
-                <span class="input-group-text">$</span>
-                <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-                <span class="input-group-text">.00</span>
-              </div>
-              
-              <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username">
-                <span class="input-group-text">@</span>
-                <input type="text" class="form-control" placeholder="Server" aria-label="Server">
+                <input type="text" placeholder="Time Reservation" class="form-control" id="timeinput" value="" aria-label="Disabled input example" aria-describedby="basic-addon" disabled>
               </div>
               
               <div class="input-group">
-                <span class="input-group-text">With textarea</span>
+                <span class="input-group-text">Reservation Notes</span>
                 <textarea class="form-control" aria-label="With textarea"></textarea>
               </div>
                </span>
@@ -112,42 +100,42 @@
           <div class="container">
             <div class="row">
               <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053">16:00 - 16:30</button>
+                <button class="btn btn-secondary btn-time" data-dismiss="modal" value="16:00 - 16:30" type="button" style="background-color: #D75053">16:00 - 16:30</button>
               </div>
               <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053">16:30 - 17:00</button>
+                <button type="button" class="btn btn-secondary btn-time" data-dismiss="modal" value="16:30 - 17:00" style="background-color: #D75053">16:30 - 17:00</button>
               </div>
               <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053; height:2cm">17:00 - 17:30</button>
-              </div>
-            </div>
-            <div class="row" style="margin-top: 2cm">
-              <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053">17:30 - 18:00</button>
-              </div>
-              <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053">18:30 - 19:00</button>
-              </div>
-              <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053; height:2cm">19:30 - 20:00</button>
+                <button type="button" class="btn btn-secondary btn-time"  data-dismiss="modal" value="17:00 - 17:30" style="background-color: #D75053; height:2cm">17:00 - 17:30</button>
               </div>
             </div>
             <div class="row" style="margin-top: 2cm">
               <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053">20:00 - 20:30</button>
+                <button type="button" class="btn btn-secondary btn-time" data-dismiss="modal" value="17:30 - 18:00" style="background-color: #D75053">17:30 - 18:00</button>
               </div>
               <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053">20:30 - 21:00</button>
+                <button type="button" class="btn btn-secondary btn-time" data-dismiss="modal" value="18:30 - 19:00" style="background-color: #D75053">18:30 - 19:00</button>
               </div>
               <div class="col-4 d-grid gap-2 mx-auto">
-                <button type="button" class="btn btn-secondary" style="background-color: #D75053; height:2cm">21:00 - 21:30</button>
+                <button type="button" class="btn btn-secondary btn-time" data-dismiss="modal" value="19:30 - 20:00" style="background-color: #D75053; height:2cm">19:30 - 20:00</button>
+              </div>
+            </div>
+            <div class="row" style="margin-top: 2cm">
+              <div class="col-4 d-grid gap-2 mx-auto">
+                <button type="button" class="btn btn-secondary btn-time" data-dismiss="modal" value="20:00 - 20:30" style="background-color: #D75053">20:00 - 20:30</button>
+              </div>
+              <div class="col-4 d-grid gap-2 mx-auto">
+                <button type="button" class="btn btn-secondary btn-time" data-dismiss="modal" value="20:30 - 21:00" style="background-color: #D75053">20:30 - 21:00</button>
+              </div>
+              <div class="col-4 d-grid gap-2 mx-auto">
+                <button type="button" class="btn btn-secondary btn-time" data-dismiss="modal" value="21:00 - 21:30" style="background-color: #D75053; height:2cm">21:00 - 21:30</button>
               </div>
             </div>
           </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <a href="order"><button type="button" class="btn btn-primary" style="background-color: #D75053">Order</button></a>
+          {{-- <a href="order"><button type="button" class="btn btn-primary" style="background-color: #D75053">Order</button></a> --}}
         </div>
       </div>
     </div>
@@ -158,8 +146,16 @@
 <script>
 AOS.init();
 $('.date').datepicker({
-  forceParse: false
+  format: "yyyy/mm/dd"
+  // todayBtn: "linked"
 });
+
+$(".btn-time").click(function(){
+    console.log($(this).val());
+    $("#timeinput").val($(this).val());
+});
+
+
 </script>
 
 </html>
